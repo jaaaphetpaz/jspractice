1ad9df4191b0d1d86af6749768ede539a565bb28
jaaaphetpaz <102007135+jaaaphetpaz@users.noreply.github.com>
2024-01-31 12:28:09 -0500

    Loops review

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("hello");
+// var num1 = "red";
+// var num2 = "blue";
+// var test1 = num1 % 2;
+// var test2 = num2 % 2;
+// var result1 = test1 == 0;
+// var result2 = test2 == 0;
+
+// console.log("Is", num1, "an even number?", result1);
+// console.log("Is", num2, "an even number?", result2);
+
+// console.log(1 + num2);
+
+// var age = 30;
+
+// if (age > 65) {
+//   console.log("Pension");
+// } else if (age < 65 && age >= 18) {
+//   console.log("Salary");
+// } else if (age < 18) {
+//   console.log("Allowance");
+// } else {
+//   console.log("not valid");
+
+let counter = 20;
+
+while (counter <= 25) {
+  console.log(counter);
+  counter++;
+}
+console.log("swag");
